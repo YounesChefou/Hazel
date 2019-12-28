5e99c82c8dd7edb1468b05b4f16ee70688605c3c
3670126 <melissa.lebreton@etu.upmc.fr>
2019-12-28 19:43:46 +0100

    Ajout_cases_pouvoirs_et_ennemi

--- Graphique/main.cpp
+++ Graphique/main.cpp
@@ -8,6 +8,7 @@
 
 int main(int argc, char *argv[]){
     QApplication app(argc, argv);
+
     //Iniatilisation
 
     //Fenêtre de Jeu
@@ -27,7 +28,7 @@ int main(int argc, char *argv[]){
     QProgressBar PV(&fenetre);
     PV.setTextVisible(false);
     PV.move(30,20);
-    PV.setValue(100/*PV max Joueur*/);
+    PV.setValue(100/*PV Joueur*/);
     PV.setMinimum(0);
     PV.setMaximum(100/*PV max Joueur*/);
 
@@ -43,19 +44,54 @@ int main(int argc, char *argv[]){
     HP.setMinimum(0);
     HP.setMaximum(100/*HP max Joueur*/);
 
-    //Pouvoirs
-    //QKeyEvent Ev(QEvent::KeyPress,Qt::KeypadModifier,QString(),false,1);
+
+    //Affichage Pouvoirs
+    QSize size(30,30); //Dimension image
+
+    //Base
+    QLabel physique(&fenetre);
+    physique.setPixmap((QPixmap("physique.png")).scaled(size)); //Créer l'image
+    physique.move(1000,10);
+
+    //Feu
+    QLabel feu(&fenetre);
+    feu.setPixmap((QPixmap("flamme.png")).scaled(size));
+    feu.move(1040,10);
+
+    //Poison
+    QLabel poison(&fenetre);
+    poison.setPixmap((QPixmap("poison.png")).scaled(size)); //Créer l'image
+    poison.move(1080,10);
+
+    //Soin
+    QLabel soin(&fenetre);
+    soin.setPixmap((QPixmap("soin.png")).scaled(size)); //Créer l'image
+    soin.move(1120,10);
+
+    //Glace
+    QLabel glace(&fenetre);
+    glace.setPixmap((QPixmap("glace.png")).scaled(size)); //Créer l'image
+    glace.move(1160,10);
 
 
-//    TEST OpenClassroom
-//    QString att = "Feu";
-//    QPushButton bouton(att, &fenetre);
-//    QFont it(att, -1,75,false);
-//    bouton.move(150,75);
-//    bouton.setCursor(Qt::PointingHandCursor);
-//    bouton.setFont(it);
-//    bouton.setToolTip("Attaque");
-//    bouton.setIcon(QIcon("flamme.png"));
+    //Affichage Ennemi
+    QLabel ennemi(&fenetre);//Type Ennemi ou Nom
+    ennemi.setText("Nom");
+    ennemi.move(30,610);
+
+    //Barre de Vie ennemi
+    QLabel EPV(&fenetre);
+    EPV.setText("PV");
+    EPV.move(10,635);
+
+    QProgressBar ENPV(&fenetre);
+    ENPV.setTextVisible(false);
+    ENPV.move(30,630);
+    ENPV.setValue(100/*PV ennemi*/);
+    ENPV.setMinimum(0);
+    ENPV.setMaximum(100/*PV max ennemi*/);
+
+
     fenetre.show();
     return app.exec();
 }
