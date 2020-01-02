e8aa0fd66f7f5cb6c291f62a631b6832d49f233c
lephoquebleu <younes.chefou@gmail.com>
2020-01-02 17:20:10 +0100

    Test du changement de sprite, cliquer dessus pour changer

--- Graphique/SpriteJoueur.cc
+++ Graphique/SpriteJoueur.cc
@@ -1,5 +1,6 @@
 #include "SpriteJoueur.hh"
 
+
 //Permet de déplacer le sprite
 void SpriteJoueur::keyPressEvent(QKeyEvent *event)
 {
@@ -17,6 +18,45 @@ void SpriteJoueur::keyPressEvent(QKeyEvent *event)
             setPos(x(), y()+10);
             break;
         default:
+            break;
+    }
+}
+
+//Test du changement de sprite
+void SpriteJoueur::mousePressEvent(QGraphicsSceneMouseEvent *event){
+    static int d = 0;
+    if(event->button() == Qt::LeftButton){
+        switch(d){
+            case 0:
+            this->changerSprite('S');
+            break;
+            case 1:
+            this->changerSprite('G');
+            break;
+            case 2:
+            this->changerSprite('F');
+            break;
+        }
+    }
+
+    d++;
+    if(d >= 3) d = 0;
+}
+//Permet de changer de sprite
+void SpriteJoueur::changerSprite(char typeFumee){
+    QPixmap spriteStandard("../Ressources/spriteGenerique.png");
+    QPixmap spriteGlace("../Ressources/fumeeBleue.png");
+    QPixmap spriteFeu("../Ressources/fumeeRouge.png");
+
+    switch(typeFumee){
+        case 'S':
+            this->setPixmap(spriteStandard);
+            break;
+        case 'G':
+            this->setPixmap(spriteGlace);
+            break;
+        case 'F':
+            this->setPixmap(spriteFeu);
             break;
     }
 }
--- Graphique/SpriteJoueur.hh
+++ Graphique/SpriteJoueur.hh
@@ -1,9 +1,15 @@
 #include <QGraphicsItem>
 #include <QKeyEvent>
+#include <QMouseEvent>
+#include <QGraphicsSceneMouseEvent>
 
 class SpriteJoueur : public QGraphicsPixmapItem{
 public:
+//    static QPixmap spriteStandard;
+//    static QPixmap spriteGlace;
+//    static QPixmap spriteFeu;
     SpriteJoueur(QPixmap &p):QGraphicsPixmapItem(p){}
     void keyPressEvent(QKeyEvent *event);
+    void mousePressEvent(QGraphicsSceneMouseEvent *event);
+    void changerSprite(char typeFumee);
 };
-
